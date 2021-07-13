44f984409d3994b9fa36f7375b1ff169eca54348
John <johnobiofiong1@gmail.com>
2021-07-13 06:56:39 +0100

    initial commit

---
+++ Components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+
+const Layout = ({children}) =>{
+    return (
+        <div className = "">
+            {children}
+        </div>
+    )
+}
+
+export default Layout
